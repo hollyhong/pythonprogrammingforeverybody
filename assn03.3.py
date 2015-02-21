03584e7ff7e20f75bc9c738bdd3f270107b26975
Holly Hong <holly.hong@gmail.com>
2015-02-21 14:18:21 -0800

    clean up syntax

--- assn03.3.py
+++ assn03.3.py
@@ -5,7 +5,7 @@ try:
 	if score > 1 or score < 0:
 		print 'Score must be between 0 and 1'
 	else:
-		if score >=0.9:
+		if score >= 0.9:
 			print 'A'
 		elif score >= 0.8:
 			print 'B'
@@ -13,7 +13,7 @@ try:
 			print 'C'
 		elif score >= 0.6:
 			print 'D'
-		elif score < 0.6:
+		else:
 			print 'F'	
 except:
 	print 'Score must be between 0 and 1'
