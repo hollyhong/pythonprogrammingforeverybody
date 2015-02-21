1428534351f2b8c9c7b59b20a8ebdbac5985b482
Holly Hong <holly.hong@gmail.com>
2015-02-21 14:09:21 -0800

    fouth assignment complete

--- assn03.3.py
+++ assn03.3.py
@@ -2,21 +2,18 @@ rawstr = raw_input("Enter score:")
 
 try:
 	score = float(rawstr)
+	if score > 1 or score < 0:
+		print 'Score must be between 0 and 1'
+	else:
+		if score >=0.9:
+			print 'A'
+		elif score >= 0.8:
+			print 'B'
+		elif score >= 0.7:
+			print 'C'
+		elif score >= 0.6:
+			print 'D'
+		elif score < 0.6:
+			print 'F'	
 except:
-	score = -1
-
-if score = 	
-
-if score <= 1.0 and >=0.9:
-	print 'A'
-elif score >= 0.8 and < 0.9:
-	print 'B'
-elif score >= 0.7 and < 0.8:
-	print 'C'
-elif score >= 0.6 and < 0.7:
-	print 'D'
-elif score < 0.6:
-	print 'F'
-else:
-	print 'Score must be 0.0 to 1.0'	
-
+	print 'Score must be between 0 and 1'
